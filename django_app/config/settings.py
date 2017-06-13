8d68d2a476b97b771f0edff476219244e9c046c9
junho Kwon <oilvers0904@gmail.com>
2017-06-13 12:18:47 +0900

    STATIC_DIR,STATICFILES_DIR에 세팅

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATICFIELS_DIRS = [
+    STATIC_DIR,
+]
 
+
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
